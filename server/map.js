020bee6324e1f548d6f61c7ac7b0451e2225f782
Deepak Prabhakara <deepak.prabhakara@gmail.com>
2015-11-08 01:23:38 +0000

    reorg

--- server/map.js
+++ server/map.js
@@ -43,109 +43,93 @@ function checkForTracker(url) {
   if (result === null) {
     var newTracker = urlRegExp.exec(url);
     if (newTracker && newTracker.length > 1) {
-      //console.log('newTracker=', newTracker.length, newTracker[1]);
+      console.log('Found new tracker', newTracker[1]);
       return newTracker[1];
     }
   }
   return result;
 }
 
+function getPromise(msg) {
+  var promise = new Promise(function (resolve, reject) {
+
+    var list = {
+      trackers: {}
+    };
+
+    var parser = new htmlparser.Parser({
+      onopentag: function (tagname, attribs) {
+        //console.log('opentag=', tagname);
+        if (tagname === 'img') {
+          if (attribs.src && attribs.width === '1' && attribs.height === '1' || attribs.width === '0' && attribs.height === '0') {
+            //console.log('name, attribs=', tagname, attribs.width, attribs.height, attribs.src);
+            var tracker = checkForTracker(attribs.src);
+
+            //console.log('tracker=', tracker);
+            if (tracker !== null) {
+
+              var count = list.trackers[tracker];
+              //console.log('count=', count, typeof count);
+              if (count === null || typeof count !== 'number') {
+                count = 0;
+              }
+              count += 1;
+              list.trackers[tracker] = count;
+              /*lateRet.name = 'tidList';
+              lateRet.key = msg.threadId;
+              lateRet.value = { list: result };*/
+            }
+          }
+        }
+      },
+      onend: function () {
+        //console.log('ending parsing!', arguments);
+        var lateRet = [];
+        //console.log('Thats it?!');
+        console.log('Found trackers', list);
+        if (Object.keys(list.trackers).length > 0) {
+          lateRet.push({
+            name: 'idList',
+            key: msg.id,
+            value: { list: list }
+          });
+          lateRet.push({
+            name: 'tidList',
+            key: msg.threadId,
+            value: { list: list }
+          });
+        }
+        resolve(lateRet);
+      }
+    }, { decodeEntities: false });
+    parser.write(msg.htmlBody);
+    parser.end();
+
+  });
+  return promise;
+}
+
 // Entry point for DAG node
 module.exports = function (got) {
-  console.log('MAP: mapping... got', got);
+  //console.log('MAP: mapping... got', got);
   const inData = got['in'];
-  const withData = got['with'];
+  //const withData = got['with'];
 
   console.log('MAP: mapping...');
   //console.log('MAP: inData...', inData);
-  console.log('MAP: withData...', withData);
+  //console.log('MAP: withData...', withData);
   var ret = [];
   for (var d of inData.data) {
-    console.log('MAP: key: ', d.key);
+    //console.log('MAP: key: ', d.key);
     if (d.value) {
       try {
         var msg = JSON.parse(d.value);
-        console.log('MAP: msg.ID: ', msg.id, msg.threadId);
+        //console.log('MAP: msg.ID: ', msg.id, msg.threadId);
 
         if (msg.htmlBody && msg.htmlBody.length > 0 /*&& msg.htmlBody.indexOf('adsafeprotected.com') > 0*/) {
+          var promise = getPromise(msg);
 
-          var promise = new Promise(function (resolve, reject) {
-
-            var list = {
-              trackers: {}
-            };
-
-            var parser = new htmlparser.Parser({
-              onopentag: function (tagname, attribs) {
-                //console.log('opentag=', tagname);
-                if (tagname === 'img') {
-                  if (attribs.src && attribs.width === '1' && attribs.height === '1' || attribs.width === '0' && attribs.height === '0') {
-                    console.log('name, attribs=', tagname, attribs.width, attribs.height, attribs.src);
-                    var tracker = checkForTracker(attribs.src);
-
-                    console.log('tracker=', tracker);
-                    if (tracker !== null) {
-
-                      var count = list.trackers[tracker];
-                      console.log('count=', count, typeof count);
-                      if (count === null || typeof count !== 'number') {
-                        count = 0;
-                      }
-                      count += 1;
-                      list.trackers[tracker] = count;
-                      /*lateRet.name = 'tidList';
-                      lateRet.key = msg.threadId;
-                      lateRet.value = { list: result };*/
-                    }
-                  }
-                }
-              },
-              onend: function () {
-                console.log('ending parsing!', arguments);
-                var lateRet = [];
-                console.log('Thats it?!');
-                console.log('resolving', list);
-                if (Object.keys(list.trackers).length > 0) {
-                  lateRet.push({
-                    name: 'idList',
-                    key: msg.id,
-                    value: { list: list }
-                  });
-                  lateRet.push({
-                    name: 'tidList',
-                    key: msg.threadId,
-                    value: { list: list }
-                  });
-                }
-                resolve(lateRet);
-              }
-            }, { decodeEntities: false });
-            parser.write(msg.htmlBody);
-            parser.end();
-
-            /*var url = trackerRegExp.exec(msg.htmlBody);
-            if (url && url.length > 1) {
-              //console.log('found url = ', url.length, url[1]);
-              var result = checkForTracker(url[1]);
-              console.log('result=', result);
-              if (result !== null) {
-                ret.push({
-                  name: 'idList',
-                  key: msg.id,
-                  value: { list: result }
-                });
-
-                ret.push({
-                  name: 'tidList',
-                  key: msg.threadId,
-                  value: { list: result }
-                });
-              }
-            }*/
-
-          });
-
-          console.log('pushing promise', promise);
+          //console.log('pushing promise', promise);
           ret.push(promise);
         }
       }
@@ -160,7 +144,7 @@ module.exports = function (got) {
     }
   }
   console.log('MAP: mapped: ', ret);
-  console.log('MAP: mapped length: ', ret.length);
+  //console.log('MAP: mapped length: ', ret.length);
   if (ret.length === 0) {
     return ret;
   } else if (ret.length === 1) {

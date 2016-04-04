d51594d89fb27672e9408fe1fcf61914171c90da
Randal Pinto <randal@redsift.io>
2016-04-04 15:17:22 +0100

    New sift.json format.

--- frontend/controller.js
+++ frontend/controller.js
@@ -1,5 +1,5 @@
 'use strict';
-/* globals include, Sift */
+/* globals include, Redsift, Sift */
 
 /**
  * sift-pixel-tracker. Frontend entry point
@@ -132,3 +132,37 @@ Sift.Storage.addUpdateListener('_tid.list', function (value) {
     });
   }
 });
+
+/**
+ * Application event handlers
+ */
+
+Redsift.Client.loadMessageListView = function (listInfo, supportedTemplates) {
+  console.log('sift-pixel-tracker: loadMessageListView: ', listInfo, supportedTemplates);
+  return Redsift.Client.loadThreadListView(listInfo, supportedTemplates);
+};
+
+Redsift.Client.loadThreadListView = function (listInfo, supportedTemplates) {
+  console.log('sift-pixel-tracker: loadThreadListView: ', listInfo, supportedTemplates);
+
+  var ret = {
+    template: '003_list_common_img'
+  };
+  if (listInfo && listInfo.trackers) {
+    var subtitle = '';
+    Object.keys(listInfo.trackers).forEach(function (tracker) {
+      if (subtitle.length > 0) {
+        subtitle += ', ';
+      }
+      subtitle += tracker;
+    });
+
+    ret.value = {
+      image: {
+        url: 'frontend/assets/fa-eye@2x.png'
+      },
+      subtitle: subtitle
+    };
+  }
+  return ret;
+};

69639eb78ec8b2c7b76176953898e7b686277f79
Christos Vontas <christos@redsift.io>
2016-08-02 19:53:01 +0100

    finished with controller

--- frontend/src/scripts/controller.js
+++ frontend/src/scripts/controller.js
@@ -1,175 +1,148 @@
-'use strict';
-/* globals include, Redsift, Sift */
-
 /**
- * sift-pixel-tracker. Frontend entry point
+ * sift-pixel-tracker: frontend controller entry point.
  *
- * Copyright (c) 2015 Redsift Limited. All rights reserved.
+ * Copyright (c) 2016 Redsift Limited. All rights reserved.
  */
 
-var summaryView = false;
+import { SiftController, registerSiftController } from '@redsift/sift-sdk-web';
 
-function getAllValues() {
-  return new Promise(function (resolve, reject) {
-    Sift.Storage.getAll({ bucket: '_email.tid' }).then(function (results) {
-      //console.log('allValues=', results);
-      var graph = {};
-      graph.name = 'Trackers';
-      graph.children = [];
-      var trackers = {};
-      results.forEach(function (result) {
-        try {
-          var obj = JSON.parse(result.value);
-          Object.keys(obj.detail.trackers).forEach(function (tracker) {
-            var name = obj.detail.trackers[tracker].name.toLowerCase();
-            //console.log('tracker=', tracker);
-            //console.log('tracker name=', name);
-            var trackerHash = trackers[name];
-            if (!trackerHash) {
-              trackerHash = {};
-              trackerHash.count = 0;
-              trackers[name] = trackerHash;
-            }
-            console.log('obj.detail.trackers[tracker]', obj.detail.trackers[tracker]);
-            if (typeof obj.detail.trackers[tracker] === 'number') {
-              trackerHash.count += obj.detail.trackers[tracker];
-            } else {
-              if (obj.detail.trackers[tracker].count) {
-                trackerHash.count += obj.detail.trackers[tracker].count;
-              }
-              if (obj.detail.trackers[tracker].name) {
-                trackerHash.name = obj.detail.trackers[tracker].name;
-              }
-              if (obj.detail.trackers[tracker].id) {
-                trackerHash.id = obj.detail.trackers[tracker].id;
-              }
-            }
-          });
-        } catch (err) {
-          console.error('getAllValues err', err);
-        }
-      });
+export default class PixelTrackerController extends SiftController {
+  constructor() {
+    // You have to call the super() method to initialize the base class.
+    super();
+
+    this._summaryView = false;
+
+
+    // // This is how you subscribe to the storage  (to use class variables and functions don't
+    // // forget to bind the 'this' pointer!):
+    // this.storage.subscribe(['year', 'month', 'day'], this.onStorageUpdate.bind(this));
+    // // Subscribe to your custom events from the Sift view like so (to use class variables and functions don't
+    // // forget to bind the 'this' pointer!):
+    // this.view.subscribe('currency', this.onCurrencyChange.bind(this));
+    this.storage.subscribe('_email.tid', this.threadUpdates.bind(this))
+  }
+
+  // Function: loadView
+  // Description: Invoked when a Sift has transitioned to a final size class or when its storage has been updated
+  // Parameters:
+  // @value: {
+  //          sizeClass: {
+  //            previous: {width: 'small'|'medium'|'full', height: 'medium'|'full'},
+  //            current:  {width: 'small'|'medium'|'full', height: 'medium'|'full'}
+  //          },
+  //          type: 'email-compose'|'email-detail'|'summary',
+  //          params: {<object>}
+  //        }
+  // return: null or {html:'<string>', data: {<object>}}
+  // @resolve: function ({html:'<string>', data: {<object>})
+  // @reject: function (error)
+  threadUpdates(value){
+    if (!this._summaryView) return;
+
+    console.log('sift-pixel-tracker: storage updated: ', value);
+    this._getAllValues().then(graph => this.publish('graph', graph) );
+  }
+
+  loadView(state) {
+    console.log('sift-pixel-tracker: loadView', state);
+    var result = {
+      html: 'view.html'
+    };
+
+    if (!state.params || Object.keys(state.params).length === 0) {
+      if (state.type === 'summary') {
+        // return async
+        this._summaryView = true;
+        result.data = this._getAllValues().then(g => ({ graph: g }))
+      } else {
+        result.data = {
+          graph: {
+            'name': 'no-trackers-found',
+            'children': []
+          }
+        };
+      }
+    } else {
+      var graph = {
+        name: 'Trackers',
+        children: []
+      };
+      var trackers = state.params.detail.trackers;
 
       Object.keys(trackers).forEach(function (tracker) {
-        //console.log('graphing:', tracker, trackers[tracker]);
-        graph.children.push({ id: trackers[tracker].id, count: trackers[tracker].count, name: trackers[tracker].name });
+        if (typeof trackers[tracker] === 'number') {
+          graph.children.push({ name: tracker, count: trackers[tracker] });
+        } else {
+          graph.children.push({ name: tracker, count: trackers[tracker].count, url: trackers[tracker].url });
+        }
       });
 
-      console.log('graph async=', graph, resolve, reject);
-      resolve(graph);
-    }).catch(function (err) {
-      console.log('Got error', err);
-      reject(err);
-    });
-  });
-}
-
-function loadSummaryView(value, resolve, reject) {
-  summaryView = true;
-  var result = {
-    html: 'frontend/view.html'
-  };
-
-  getAllValues().then(function (graph) {
-    result.data = { graph: graph };
-    resolve(result);
-  }).catch(reject);
-}
-
-// Function: loadView
-// Description: Invoked when a Sift has transitioned to a final size class
-// Parameters:
-// @value: {
-//          sizeClass: {
-//            previous: {width: 'compact'|'full', height: 'compact'|'full'},
-//            current:  {width: 'compact'|'full', height: 'compact'|'full'}
-//          },
-//          type: 'compose'|'email-detail'|'summary',
-//          params: {<object>}
-//        }
-// return: null or {html:'<string>', data: {<object>}}
-// @resolve: function ({html:'<string>', data: {<object>}})
-// @reject: function (error)
-Sift.Controller.loadView = function (value, resolve, reject) {
-  console.log('sift-pixel-tracker: loadView', value);
-  var result = {
-    html: 'frontend/view.html'
-  };
-
-  if (!value.params || Object.keys(value.params).length === 0) {
-    if (value.type === 'summary') {
-      // return async
-      loadSummaryView(value, resolve, reject);
-    } else {
-      result.data = {
-        graph: {
-          'name': 'no-trackers-found',
-          'children': [
-          ]
-        }
-      };
+      result.data = { graph: graph };
     }
-  } else {
-    var graph = {};
-    graph.name = 'Trackers';
-    graph.children = [];
-    var trackers = value.params.detail.trackers;
 
-    Object.keys(trackers).forEach(function (tracker) {
-      if (typeof trackers[tracker] === 'number') {
-        graph.children.push({ name: tracker, count: trackers[tracker] });
-      } else {
-        graph.children.push({ name: tracker, count: trackers[tracker].count, url: trackers[tracker].url });
-      }
-    });
-
-    result.data = { graph: graph };
+    console.log('result sync=', result);
+    // Synchronous return
+    return result;
   }
 
-  console.log('result sync=', result);
-  // Synchronous return
-  return result;
-};
+  _getAllValues() {
+    return new Promise(function (resolve, reject) {
+      this.storage.getAll({ bucket: '_email.tid' }).then(function (results) {
+        //console.log('allValues=', results);
+        var graph = {
+          name: 'Trackers',
+          children: []
+        }
+        var trackers = {};
+        results.forEach(function (result) {
+          try {
+            var obj = JSON.parse(result.value);
+            Object.keys(obj.detail.trackers).forEach(function (tracker) {
+              var name = obj.detail.trackers[tracker].name.toLowerCase();
+              //console.log('tracker=', tracker);
+              //console.log('tracker name=', name);
+              var trackerHash = trackers[name];
+              if (!trackerHash) {
+                trackerHash = {};
+                trackerHash.count = 0;
+                trackers[name] = trackerHash;
+              }
+              console.log('obj.detail.trackers[tracker]', obj.detail.trackers[tracker]);
+              if (typeof obj.detail.trackers[tracker] === 'number') {
+                trackerHash.count += obj.detail.trackers[tracker];
+              } else {
+                if (obj.detail.trackers[tracker].count) {
+                  trackerHash.count += obj.detail.trackers[tracker].count;
+                }
+                if (obj.detail.trackers[tracker].name) {
+                  trackerHash.name = obj.detail.trackers[tracker].name;
+                }
+                if (obj.detail.trackers[tracker].id) {
+                  trackerHash.id = obj.detail.trackers[tracker].id;
+                }
+              }
+            });
+          } catch (err) {
+            console.error('getAllValues err', err);
+          }
+        });
 
-Sift.Storage.addUpdateListener('_email.tid', function (value) {
-  if (summaryView) {
-    console.log('sift-pixel-tracker: storage updated: ', value);
-    getAllValues().then(function (graph) {
-      Sift.Controller.notifyListeners('graph', graph);
+        Object.keys(trackers).forEach(function (tracker) {
+          //console.log('graphing:', tracker, trackers[tracker]);
+          graph.children.push({ id: trackers[tracker].id, count: trackers[tracker].count, name: trackers[tracker].name });
+        });
+
+        console.log('graph async=', graph, resolve, reject);
+        resolve(graph);
+      }).catch(function (err) {
+        console.log('Got error', err);
+        reject(err);
+      });
     });
   }
-});
 
-/**
- * Application event handlers
- */
+}
 
-Redsift.Client.loadMessageListView = function (listInfo, supportedTemplates) {
-  console.log('sift-pixel-tracker: loadMessageListView: ', listInfo, supportedTemplates);
-  return Redsift.Client.loadThreadListView(listInfo, supportedTemplates);
-};
-
-Redsift.Client.loadThreadListView = function (listInfo, supportedTemplates) {
-  console.log('sift-pixel-tracker: loadThreadListView: ', listInfo, supportedTemplates);
-
-  var ret = {
-    template: '003_list_common_img'
-  };
-  if (listInfo && listInfo.trackers) {
-    var subtitle = '';
-    Object.keys(listInfo.trackers).forEach(function (tracker) {
-      if (subtitle.length > 0) {
-        subtitle += ', ';
-      }
-      subtitle += tracker;
-    });
-
-    ret.value = {
-      image: {
-        url: 'frontend/assets/fa-eye@2x.png'
-      },
-      subtitle: subtitle
-    };
-  }
-  return ret;
-};
+// FIXXME: how can we automate the registration without developer interaction
+registerSiftController(new PixelTrackerController());
---
+++ frontend/src/scripts/email-client-controller.js
@@ -0,0 +1,33 @@
+import { EmailClientController, registerEmailClientController } from '@redsift/sift-sdk-web';
+
+export default class PixelTrackerEmailClientController extends EmailClientController {
+  constructor() {
+    super();
+  }
+
+  loadThreadListView (listInfo) {
+    console.log('sift-pixel-tracker: loadThreadListView: ', listInfo);
+    let ret = {
+      template: '003_list_common_img'
+    };
+    if (listInfo && listInfo.trackers) {
+      let subtitle = '';
+      Object.keys(listInfo.trackers).forEach(function (tracker) {
+        if (subtitle.length > 0) {
+          subtitle += ', ';
+        }
+        subtitle += tracker;
+      });
+
+      ret.value = {
+        image: {
+          url: 'public/assets/fa-eye@2x.png'
+        },
+        subtitle: subtitle
+      };
+    }
+    return ret;
+  };
+}
+
+registerEmailClientController(new PixelTrackerEmailClientController());
